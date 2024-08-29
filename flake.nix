54eeba9f9ac6fc9b239d7c6d7594c952f11a08b9
LilleAila <olai.solsvik@gmail.com>
2024-08-29 08:43:36 +0200

    la til nix flake og gitignore

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Samleside for IT2, 2024-2025, Python.";
+
+  # Package repository
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      # Funksjoner for å gjøre ting enklere
+      lib = nixpkgs.lib;
+      systems = lib.systems.flakeExposed;
+      pkgsFor = lib.genAttrs systems (system: import nixpkgs { inherit system; });
+      forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    in
+    {
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            # Verktøy for nix
+            nixfmt-rfc-style
+            nixd
+            # Verktøy for python
+            black
+            pyright
+          ];
+        };
+      });
+
+    };
+}

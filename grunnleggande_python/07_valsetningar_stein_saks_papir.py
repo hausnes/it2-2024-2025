ce574a1b2aab05bc62c3a29c86c4d42543785939
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-08-29 08:54:44 +0200

    Oppgåver rundt valgsetningar

--- grunnleggande_python/07_valsetningar_stein_saks_papir.py
+++ grunnleggande_python/07_valsetningar_stein_saks_papir.py
@@ -11,3 +11,22 @@ print(tilfeldigTall)
 
 tilfeldigVal = random.choice(["Karakter 1", "Karakter 2", "Karakter 3", "Karakter 4", "Karakter 5", "Karakter 6"])
 print(tilfeldigVal)
+
+# Sjølve spelet -- tips til å kome i gang med ein litt meir avansert løysning
+poengSpelar = 0
+poengDatamaskin = 0
+
+print(f"""
+-------------------------------
+Velkommen til stein-saks-papir!
+Forklar reglar...
+--------------------------------
+""")
+
+while poengSpelar < 3 or poengDatamaskin < 3:
+    valg = input("Stein, saks eller papir? ")
+    # ...
+
+print(""" 
+      Oppsummering av resultat...
+""")

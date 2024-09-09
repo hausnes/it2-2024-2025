b0620fdd10bf80c746e7df42e811170f1322016b
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-09-09 12:02:06 +0200

    Create 09-lokker-for.py

---
+++ grunnleggande_python/09-lokker-for.py
@@ -0,0 +1,30 @@
+# Sjekkar om tala æ, ø eller å er i input frå brukar:
+innlest = input("Input: ")
+for bokstav in innlest.lower():
+    if bokstav == "æ" or bokstav == "ø" or bokstav == "å":
+        print("Ikkje lov til å bruke æ, ø eller å.")
+
+# Alternativ versjon
+# Sjekkar om tala æ, ø eller å er i input frå brukar:
+innlest = input("Input: ")
+forbudte_bokstaver = {"æ", "ø", "å"}
+if any(bokstav in forbudte_bokstaver for bokstav in innlest.lower()):
+    print("Ikkje lov til å bruke æ, ø eller å.")
+
+# Stein, saks, papir og tilfeldigheiter
+import random
+
+antallStein = 0
+antallSaks = 0
+antallPapir = 0
+
+for kast in range(4):
+    resultat = random.choice(["stein", "saks", "papir"])
+    if resultat == "stein":
+        antallStein += 1
+    elif resultat == "saks":
+        antallSaks += 1
+    else:
+        antallPapir += 1
+    
+print(f"Stein: {antallStein},\n Saks: {antallSaks},\n Papir: {antallPapir}")

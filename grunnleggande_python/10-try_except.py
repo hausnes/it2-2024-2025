282a7af7528c3b58ea6f6b8b870b400e991f26e7
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-09-12 13:41:44 +0200

    Update 10-try_except.py

--- grunnleggande_python/10-try_except.py
+++ grunnleggande_python/10-try_except.py
@@ -1,16 +1,16 @@
 # Eksempel 1, frå boka, kap. 1C, "validering av input"
-gyldig = False
+# gyldig = False
 
-while not gyldig:
-    tall = input("Skriv et tall: ")
+# while not gyldig:
+#     tall = input("Skriv et tall: ")
 
-    try:
-        tall = int(tall)
-        gyldig = True
-    except ValueError:
-        print("Du må skrive inn et heltall.")
+#     try:
+#         tall = int(tall)
+#         gyldig = True
+#     except ValueError:
+#         print("Du må skrive inn et heltall.")
 
-print(f"Du skrev inn {tall}.")
+# print(f"Du skrev inn {tall}.")
 
 # Eksempel 2, med litt utvidelsar
 gyldig = False
@@ -20,7 +20,9 @@ try:
         tall = input("Skriv et positivt tall: ")
 
         try:
+            # tester for ...
             tall = int(tall)
+            # tester for ...
             if tall > 0:
                 gyldig = True
             else:

195ba522025fa01ab0a2c9af331d9265e4d8e02a
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-11-07 09:49:29 +0100

    Omorganisering

---
+++ pygame/smidig-løysingsforslag/oppg-3-4-2.py
@@ -0,0 +1,9 @@
+'''
+3.4.2
+Lag et program med følgende spesifikasjoner
+• Vinduet skal ha størrelsen 500x500
+• En ball med diameter 40 piksler skal gå rundt i sirkel inni
+vinduet
+• Sirkelen skal ha en diameter på 400 piksler og være sentrert i
+vindue
+'''

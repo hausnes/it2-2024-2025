073b05ef5566d8096b3d2a82de46a4ab10a62668
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-01-20 13:00:57 +0100

    Lagt til eks. med json.dumps med indent

--- datasett/grunnleggande/eks-JSON.py
+++ datasett/grunnleggande/eks-JSON.py
@@ -5,12 +5,13 @@ filnavn = "data/land.json"
 with open(filnavn, encoding="utf-8") as fil:
   data = json.load(fil)
 
-# print(data) # Alt
+print(data) # Alt
+# print(json.dumps(data, indent=4)) # "Finare" utskrift til terminalen, eventuelt i nettlesar eller VS Code
 # print(data["region"]) # Berre Skandinavia
 # print(data["land"]) # Alt som ligg under land
 # print(data["land"][0]["navn"]) # ..fordi det under land ligg ei liste..
 
-for x in data["land"]:
-    print(f'Landet {x["navn"]} har fylgjande byar:')
-    for y in range(len(x["byer"])):
-        print(f'\t{x["byer"][y]}')
+# for x in data["land"]:
+#     print(f'Landet {x["navn"]} har fylgjande byar:')
+#     for y in range(len(x["byer"])):
+#         print(f'\t{x["byer"][y]}')

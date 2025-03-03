d077aea7f3383a354cc50d254cddd51eddef60c7
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2025-03-03 15:55:17 +0100

    Eksempel på EXE-filer

---
+++ temp/exe/program.py
@@ -0,0 +1,27 @@
+import tkinter as tk            # font er en submodul, 
+from tkinter import font        # som ikke kan kalles direkte med tk.
+from PIL import Image, ImageTk  # husk pip install Pillow
+from pathlib import Path
+
+def si_noe():
+    print("Noe!")
+
+root = tk.Tk()
+
+# Sett standard font for alle widgets i applikasjonen
+root.option_add("*Font", "Arial 20")
+
+tk.Label(root, text="Hallo!").pack()
+tk.Label(root, text="Hallo igjen!").pack()
+tk.Button(root, text="Trykk på meg!", command=si_noe).pack()
+
+# Legg til et bilde, krever from PIL import Image, ImageTk
+# bilde = Image.open(Path(__file__).resolve().parent.joinpath("tkinter.jpg"))
+bilde = Image.open("tkinter.jpg") # Alternativ til linje over
+bilde = bilde.resize((150, 150), Image.Resampling.LANCZOS)
+bilde = ImageTk.PhotoImage(bilde)
+tk.Label(root, image=bilde).pack()
+
+root.mainloop()
+
+# Smidig IT-2 © TIP AS, 2024
